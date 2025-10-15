6f18e82fedf168b1a708c8a2c3e34f605eb73015
ghenriqf <gabrielhenriquefreitas28@gmail.com>
2025-10-15 14:33:51 -0300

    feat: criar entidade user

---
+++ src/main/java/com/ghenriqf/login_auth_api/domain/user/User.java
@@ -0,0 +1,25 @@
+package com.ghenriqf.login_auth_api.domain.user;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Table(name = "users")
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    private String name;
+    private String email;
+    private String password;
+}
