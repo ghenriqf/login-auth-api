212901f639654e5e13cbd187a6e4f0b1987fa8f1
ghenriqf <gabrielhenriquefreitas28@gmail.com>
2025-10-18 16:33:02 -0300

    feat: criar user details service

---
+++ src/main/java/com/ghenriqf/login_auth_api/service/AuthorizationService.java
@@ -0,0 +1,24 @@
+package com.ghenriqf.login_auth_api.service;
+
+
+import com.ghenriqf.login_auth_api.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthorizationService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public AuthorizationService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByEmail(username).orElseThrow(() -> new UsernameNotFoundException(username));
+    }
+}
