e9e83c006d35a77683866e0c59561980c204f257
ghenriqf <gabrielhenriquefreitas28@gmail.com>
2025-10-21 17:03:30 -0300

    feat: criar security filer inicial

--- src/main/java/com/ghenriqf/login_auth_api/infra/security/SecurityFilter.java
+++ src/main/java/com/ghenriqf/login_auth_api/infra/security/SecurityFilter.java
@@ -1,5 +1,32 @@
 package com.ghenriqf.login_auth_api.infra.security;
 
-public class SecurityFilter {
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.apache.logging.log4j.util.Strings;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
 
+import java.io.IOException;
+import java.util.Optional;
+
+@Component
+public class SecurityFilter extends OncePerRequestFilter {
+
+    private final TokenService tokenService;
+
+    public SecurityFilter(TokenService tokenService) {
+        this.tokenService = tokenService;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String authorizeHeader = request.getHeader("Authorization");
+
+        if (Strings.isEmpty(authorizeHeader) && authorizeHeader.startsWith("Bearer ")) {
+            String token = authorizeHeader.substring("Bearer ".length());
+            Optional<JWTUserData> optUser = tokenService.validateToken(token);
+        }
+    }
 }
