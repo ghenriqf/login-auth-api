ecf95fad43d1ab1106fe13583db49dbabdb7960a
ghenriqf <gabrielhenriquefreitas28@gmail.com>
2025-10-21 17:02:58 -0300

    feat: implementar validação de token no TokenService

--- src/main/java/com/ghenriqf/login_auth_api/infra/security/TokenService.java
+++ src/main/java/com/ghenriqf/login_auth_api/infra/security/TokenService.java
@@ -2,12 +2,16 @@ package com.ghenriqf.login_auth_api.infra.security;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import com.ghenriqf.login_auth_api.domain.user.User;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
 import java.util.Collections;
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class TokenService {
@@ -29,5 +33,24 @@ public class TokenService {
 
     private Instant generateExpirationDate() {
         return Instant.now().plusSeconds(7200);
+    }
+
+    public Optional<JWTUserData> validateToken(String token) {
+
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+
+            DecodedJWT decode = JWT.require(algorithm)
+                    .build()
+                    .verify(token);
+
+            return Optional.of(JWTUserData.builder()
+                    .userId(UUID.fromString(decode.getClaim("userId").asString()))
+                    .email(decode.getSubject())
+                    .build());
+        } catch (JWTVerificationException e) {
+            return Optional.empty();
+        }
+
     }
 }
