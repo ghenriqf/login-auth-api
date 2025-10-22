1ba699526a78f8ffa0be43cbce6a79f595e76db5
ghenriqf <gabrielhenriquefreitas28@gmail.com>
2025-10-22 14:35:18 -0300

    feat: adicionar filtro no security config

--- src/main/java/com/ghenriqf/login_auth_api/infra/security/SecurityConfig.java
+++ src/main/java/com/ghenriqf/login_auth_api/infra/security/SecurityConfig.java
@@ -12,10 +12,17 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration // diz ao Spring que essa classe contém beans de configuração, ou seja, objetos que o Spring cria e gerencia automaticamente.
 @EnableWebSecurity // habilita o Spring Security, permitindo configurar autenticação e autorização.
 public class SecurityConfig { // configuração de segurança do Spring Security
+
+    private final SecurityFilter securityFilter;
+
+    public SecurityConfig(SecurityFilter securityFilter) {
+        this.securityFilter = securityFilter;
+    }
 
     @Bean
     // cadeia de filtros que o Spring Security vai aplicar em cada requisição HTTP.
@@ -30,6 +37,7 @@ public class SecurityConfig { // configuração de segurança do Spring Security
                         .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
                         .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
                         .anyRequest().authenticated())
+                .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
                 .build(); // gera o SecurityFilterChain, que é a cadeia de filtros de segurança usada em cada requisição
     }
 
