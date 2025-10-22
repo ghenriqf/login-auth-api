d3ed53164de4193c109a5ac7e86b3a13b2b11585
ghenriqf <gabrielhenriquefreitas28@gmail.com>
2025-10-22 15:53:22 -0300

    docs: adicionar comentarios

--- src/main/java/com/ghenriqf/login_auth_api/infra/security/TokenService.java
+++ src/main/java/com/ghenriqf/login_auth_api/infra/security/TokenService.java
@@ -23,25 +23,35 @@ public class TokenService {
         Algorithm algorithm = Algorithm.HMAC256(secret);
 
         return JWT.create()
-                .withClaim("userId", user.getId().toString())
-                .withSubject(user.getEmail())
-                .withExpiresAt(generateExpirationDate())
-                .withIssuedAt(Instant.now())
-                .sign(algorithm);
+                .withClaim("userId", user.getId().toString()) // informações personalizadas chamadas claims
+                .withSubject(user.getEmail()) // Define o subject (sub) do token campo padrão do JWT.
+                .withIssuedAt(Instant.now()) // Define a data de emissão do token
+                .withExpiresAt(generateExpirationDate()) // Define a data de expiração do token
+                .sign(algorithm); // token é assinado criptograficamente com o algoritmo definido.
     }
+    /*
+    JWT = HEADER . PAYLOAD . SIGNATURE
+    Exemplo do payload do JWT gerado
+    {
+      "userId": "f6d1bcb8-9e8d-4e0a-9efb-2f4b9c2b8a77",
+      "sub": "gabriel@email.com",
+      "iat": 1729612025,
+      "exp": 1729615625
+    }
+    */
 
+    // Gera a data de expiração do token: 2 horas (7200 segundos) a partir do momento atual
     private Instant generateExpirationDate() {
         return Instant.now().plusSeconds(7200);
     }
 
     public Optional<JWTUserData> validateToken(String token) {
-
         try {
             Algorithm algorithm = Algorithm.HMAC256(secret);
 
-            DecodedJWT decode = JWT.require(algorithm)
-                    .build()
-                    .verify(token);
+            DecodedJWT decode = JWT.require(algorithm)// cria um verificador que exige que o token seja assinado com o algoritmo e a chave correta.
+                    .build() // constrói o verificador real (JWTVerifier) a partir do builder.
+                    .verify(token); // Valida o token: assinatura, expiração e integridade; retorna DecodedJWT se válido ou lança JWTVerificationException se inválido.
 
             return Optional.of(JWTUserData.builder()
                     .userId(UUID.fromString(decode.getClaim("userId").asString()))
@@ -50,6 +60,5 @@ public class TokenService {
         } catch (JWTVerificationException e) {
             return Optional.empty();
         }
-
     }
 }
